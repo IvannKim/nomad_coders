31df4b94c21d3a5c76dbca4974bdbec24891c7f6
Peter Kim <seongjooon@gmail.com>
2018-11-30 11:13:35 +0900

    g

--- vanillaJS/index.js
+++ vanillaJS/index.js
@@ -1,9 +1,30 @@
-const age = prompt("How old r u?")
+const title = document.querySelector("#title");
 
-if (age >= 18 && age <= 21) {
-    console.log("u can drink but u should not")
-} else if (age > 21) {
-    console.log("go ahead")
-} else {
-    console.log("too young")
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handdleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handdleClick);
+
+}
+init()
+
+function handleOffline() {
+    console.log("u r offline. bye bye. check ur LAN");
+}
+
+function handleOnline() {
+    console.log("welcome back.");
+}
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
