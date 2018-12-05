8cec0588013700cebefdb4535f9e38d515b10c0a
Peter Kim <seongjooon@gmail.com>
2018-12-05 19:01:28 +0900

    b

---
+++ vanillaJS/clock.js
@@ -0,0 +1,15 @@
+const colckContainer = document.querySelector(".js-clock"),
+    clockTitle = colckContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`
+}
+
+function init() {
+    getTime();
+}
+init();
